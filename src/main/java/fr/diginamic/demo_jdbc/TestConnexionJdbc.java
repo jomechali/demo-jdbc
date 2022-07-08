79d81bb3f0c85ea96f3274cb749d1d31d9b7762e
Joseph MECHALI <mechali.joseph@gmail.com>
2022-07-08 15:40:39 +0200

    externalization of connection properties

--- src/main/java/fr/diginamic/demo_jdbc/TestConnexionJdbc.java
+++ src/main/java/fr/diginamic/demo_jdbc/TestConnexionJdbc.java
@@ -3,12 +3,23 @@ package fr.diginamic.demo_jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
+	
+	private static final String DB_URL;
+	private static final String DB_USER;
+	private static final String DB_PWD;
 
+	static {
+		ResourceBundle bundle = ResourceBundle.getBundle("db");
+		DB_URL = bundle.getString("jdbc.log.URL");
+		DB_USER = bundle.getString("jdbc.log.USER");
+		DB_PWD = bundle.getString("jdbc.log.PWD");
+	}
 	public static void main(String[] args) throws SQLException {
 
-		Connection connection = DriverManager.getConnection(args[0], args[1], args[2]);
+		Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PWD);
 
 		System.out.println(connection);
 		connection.close();
