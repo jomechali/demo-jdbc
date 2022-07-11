6c4fcf98644ea509a1edd627318d96c20ddaae64
Joseph MECHALI <mechali.joseph@gmail.com>
2022-07-11 10:47:50 +0200

    fix the DB URL

--- src/main/java/fr/diginamic/demo_jdbc/TestConnexionJdbc.java
+++ src/main/java/fr/diginamic/demo_jdbc/TestConnexionJdbc.java
@@ -20,6 +20,7 @@ public class TestConnexionJdbc {
 	public static void main(String[] args){
 
 		try(Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PWD)) {
+			System.out.println(connection);
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
